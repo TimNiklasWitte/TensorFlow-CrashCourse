e9ab7f5990d424742927673818eefb4ac2aaabe0
TimNiklasWitte <wittet@uos.de>
2023-04-23 17:50:12 +0200

    add more comments

--- 1. Model and Training/MyDenseLayer.py
+++ 1. Model and Training/MyDenseLayer.py
@@ -1,13 +1,32 @@
+"""
+Own implementation of a dense layer
+"""
+
 import tensorflow as tf
 
 class MyDenseLayer(tf.keras.layers.Layer):
 
     def __init__(self, units, activation):
+        """
+        Create a dense layer .
+
+        Args:
+            units -- number of neurons
+            activation -- activation function
+        """
+
         super(MyDenseLayer, self).__init__()
         self.units = units
         self.activation = activation
 
     def build(self, input_shape):
+        """
+        Build the weight matrix and the bias vector (parameters of this layer).
+
+        Args:
+            input_shape -- Shape of the input
+        """
+
         self.w = self.add_weight(shape=(input_shape[-1], self.units),
                         initializer='random_normal',
                         trainable=True)
@@ -16,8 +35,17 @@ class MyDenseLayer(tf.keras.layers.Layer):
                         trainable=True)
 
     @tf.function 
-    def call(self, inputs):
+    def call(self, x):
+        """
+        Determinate the output of the layer based on the input x.
+
+        Args:
+            x -- Tensor of the shape of 'input_shape' (see build)
+
+        Returns:
+            output of this layer
+        """
         
-        drive = tf.matmul(inputs, self.w) + self.b 
+        drive = tf.matmul(x, self.w) + self.b 
         y = self.activation(drive)
         return y   
--- 1. Model and Training/MyResidualLayer.py
+++ 1. Model and Training/MyResidualLayer.py
@@ -1,15 +1,46 @@
+"""
+Own implementation of a residual layer
+"""
+
 import tensorflow as tf
 
 class MyResidualLayer(tf.keras.layers.Layer):
 
     def __init__(self, units):
+        """
+        Create a residual layer.
+
+        Args:
+            units -- number of neurons
+        """
+
         super(MyResidualLayer, self).__init__()
         self.layer_1 = tf.keras.layers.Dense(units)
         self.layer_2 = tf.keras.layers.Dense(units)
 
+    def build(self, input_shape):
+        """
+        The build method on the two internal layers is called 
+        with 'input_shape' as a argument.
+
+        Args:
+            input_shape -- Shape of the input
+        """
+
+        self.layer_1.build(input_shape)
+        self.layer_2.build(input_shape)
 
     @tf.function 
     def call(self, x):
+        """
+        Determinate the output of the layer based on the input x.
+
+        Args:
+            x -- Tensor of the shape of 'input_shape' (see build)
+
+        Returns:
+            output of this layer
+        """
         
         x_old = self.layer_1(x)
         x = self.layer_2(x)
